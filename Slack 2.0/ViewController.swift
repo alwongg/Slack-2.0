4cb551e2f8fc3ed4d19d0fc9aa6750adae3f7343
alwongg <wong_1994@hotmail.com>
2017-11-11 18:33:22 -0600

    Initial commit

---
+++ Slack 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Slack 2.0
+//
+//  Created by Alex Wong on 11/11/17.
+//  Copyright © 2017 Alex Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
