6e35542cd48ff88a076449a89941d47119cd2b6b
alwongg <wong_1994@hotmail.com>
2017-11-13 16:25:22 -0600

    SW rear and front VC connected

---
+++ Slack 2.0/Controller/ChatVC.swift
@@ -0,0 +1,24 @@
+//
+//  ChatVC.swift
+//  Slack 2.0
+//
+//  Created by Alex Wong on 11/13/17.
+//  Copyright © 2017 Alex Wong. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var menuButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
