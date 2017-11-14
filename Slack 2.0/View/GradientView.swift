8e92898e0865ea2b4da6fdde45560ef679b12556
alwongg <wong_1994@hotmail.com>
2017-11-14 15:44:21 -0600

    ChannelVC UI with constraints

--- Slack 2.0/View/GradientView.swift
+++ Slack 2.0/View/GradientView.swift
@@ -11,13 +11,13 @@ import UIKit
 @IBDesignable // Real time action - work in storyboard
 class GradientView: UIView {
 
-    @IBInspectable var topColor: UIColor = UIColor.blue{
+    @IBInspectable var topColor: UIColor = #colorLiteral(red: 0.2901960784, green: 0.3019607843, blue: 0.8470588235, alpha: 1) {
         didSet{
             self.setNeedsLayout()
         }
     }
     
-    @IBInspectable var bottomColor: UIColor = UIColor.green{
+    @IBInspectable var bottomColor: UIColor = #colorLiteral(red: 0.1725490196, green: 0.831372549, blue: 0.8470588235, alpha: 1) {
         didSet{
             self.setNeedsLayout()
         }
