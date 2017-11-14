92010e64be4611154117dfdc8b8eca460a56ab19
alwongg <wong_1994@hotmail.com>
2017-11-13 21:56:38 -0600

    Update slide menu display to show 60 px from the right

--- Slack 2.0/Controller/ChannelVC.swift
+++ Slack 2.0/Controller/ChannelVC.swift
@@ -13,23 +13,8 @@ class ChannelVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
