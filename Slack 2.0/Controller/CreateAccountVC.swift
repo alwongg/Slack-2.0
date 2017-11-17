0237e36d52567c73c90a8e7871aeafdaafe7850b
alwongg <wong_1994@hotmail.com>
2017-11-17 02:20:50 -0600

    Create NotificationCenter for UserInfoChanged

--- Slack 2.0/Controller/CreateAccountVC.swift
+++ Slack 2.0/Controller/CreateAccountVC.swift
@@ -16,6 +16,7 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
     
     // MARK: - Properties
     
@@ -27,6 +28,7 @@ class CreateAccountVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -38,6 +40,30 @@ class CreateAccountVC: UIViewController {
                 profileImageView.backgroundColor = UIColor.lightGray
             }
         }
+    }
+    
+    // MARK: - Setup View
+    
+    func setupView(){
+        
+        //set spinner
+        spinner.isHidden = true
+        
+        //set placeholder text colors
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedStringKey.foregroundColor:slackPurplePlaceholder])
+        
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedStringKey.foregroundColor:slackPurplePlaceholder])
+        
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedStringKey.foregroundColor:slackPurplePlaceholder])
+        
+        //dismiss keyboard by tap
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(CreateAccountVC.handleTap))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(){
+        view.endEditing(true)
     }
     
     // MARK: - Actions
@@ -63,6 +89,10 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccount(_ sender: Any) {
+        
+        spinner.isHidden = false
+        spinner.startAnimating()
+        
         guard let name = usernameTextField.text, usernameTextField.text != ""
             else {return}
         guard let email = emailTextField.text, emailTextField.text != ""
@@ -76,8 +106,13 @@ class CreateAccountVC: UIViewController {
                     if success{
                         AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
                             if success{
-                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                
+                                self.spinner.isHidden = true
+                                self.spinner.stopAnimating()
+                                
                                 self.performSegue(withIdentifier: UNWIND, sender: nil)
+                                
+                                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
                             }
                         })
                     }
--- Slack 2.0/Utilities/Constants.swift
+++ Slack 2.0/Utilities/Constants.swift
@@ -17,6 +17,14 @@ let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
 
+// MARK: - Colors
+
+let slackPurplePlaceholder = #colorLiteral(red: 0.3254901961, green: 0.4196078431, blue: 0.7764705882, alpha: 0.5)
+
+// MARK: - Notification Constants
+
+let NOTIF_USER_DATA_DID_CHANGE = Notification.Name("notifUserDataChanged")
+
 // MARK: - Segue Constants
 
 let TO_LOGIN = "toLoginVC"
