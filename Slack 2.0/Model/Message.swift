9e7347f8e65442e23da7bfb026de2f8475046494
alwongg <wong_1994@hotmail.com>
2017-12-09 23:49:51 -0500

    Create Message struct model

---
+++ Slack 2.0/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  Slack 2.0
+//
+//  Created by Alex Wong on 12/9/17.
+//  Copyright © 2017 Alex Wong. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+}
